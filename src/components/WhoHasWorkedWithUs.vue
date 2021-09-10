a0c166cecc9a4700cf2111c1dae3407762fdac51
Marcos <marcos.franca@basis.com.br>
2021-09-10 11:18:26 -0300

    correcao grid

--- src/components/WhoHasWorkedWithUs.vue
+++ src/components/WhoHasWorkedWithUs.vue
@@ -1,7 +1,7 @@
 <template>
     <FlexCenter class="flex-col">
         <ImageTitle :src="imageTitleSrc" />
-        <div class="grid grid-cols-3 gap-5 items-center mx-8 sm:mx-14 lg:mx-32">
+        <div class="grid grid-cols-3 lg:grid-cols-6 gap-5 items-center mx-8 sm:mx-14 lg:mx-32">
             <img v-for="(item, index) in images" :key="index" :src="item" class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 sm:w-60 sm:block sm:ml-auto sm:mr-auto sm:mb-3">
         </div>
     </FlexCenter>
